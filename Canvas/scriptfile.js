ed3d58180e875aeb71b12a9a4570704fde607742
shivani <160009103+Raghuvanshish@users.noreply.github.com>
2024-10-03 07:12:26 +0530

    Create scriptfile.js

---
+++ Canvas/scriptfile.js
@@ -0,0 +1,6 @@
+ const canvas = document.getElementById('basicCanvas');
+    const context = canvas.getContext('2d');
+    
+    // Drawing a filled rectangle
+    context.fillStyle = 'blue';  
+    context.fillRect(150, 150, 100, 100);  
